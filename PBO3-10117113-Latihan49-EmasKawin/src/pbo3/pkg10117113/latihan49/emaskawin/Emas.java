d3d93236131053894c4fca801c4734592358e378
widyantoro <43356973+widyantoro@users.noreply.github.com>
2018-10-22 23:17:22 +0700

    Add files via upload

---
+++ PBO3-10117113-Latihan49-EmasKawin/src/pbo3/pkg10117113/latihan49/emaskawin/Emas.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117113.latihan49.emaskawin;
+
+/*
+ * Nama     : Eka Widyantoro
+ * NIM      : 10117113
+ * Kelas    : IF-3/PBO-3
+ * Desc     : Program untuk mehitung total yang harus dibayar hendi.
+ *            
+ */
+public class Emas {
+   public int hargaGram;
+   public double beratEmas;
+
+    public int getHargaGram() {
+        return hargaGram;
+    }
+
+    public void setHargaGram(int hargaGram) {
+        this.hargaGram = hargaGram;
+    }
+
+    public double getBeratEmas() {
+        return beratEmas;
+    }
+
+    public void setBeratEmas(double beratEmas) {
+        this.beratEmas = beratEmas;
+    }
+   
+   public double perhitungan(int hargaGram, double beratEmas) {
+       
+       return hargaGram * beratEmas ;
+       
+   } 
+}
---
+++ PBO3-10117113-Latihan49-EmasKawin/src/pbo3/pkg10117113/latihan49/emaskawin/PBO310117113Latihan49EmasKawin.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117113.latihan49.emaskawin;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+/*
+ * Nama     : Eka Widyantoro
+ * NIM      : 10117113
+ * Kelas    : IF-3/PBO-3
+ * Desc     : Program untuk mehitung total yang harus dibayar hendi.
+ *            
+ */
+public class PBO310117113Latihan49EmasKawin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        DecimalFormat mataUangIndonesia = (DecimalFormat) 
+                      DecimalFormat.getCurrencyInstance();
+        DecimalFormatSymbols formatRP = new DecimalFormatSymbols();
+        
+        formatRP.setCurrencySymbol("Rp.");
+        formatRP.setMonetaryDecimalSeparator(',');
+        formatRP.setGroupingSeparator('.');
+        
+        mataUangIndonesia.setDecimalFormatSymbols(formatRP);
+        
+        
+        Emas objEmas = new Emas();
+        objEmas.setHargaGram(570000);
+        objEmas.setBeratEmas(15.7);
+        System.out.println("=======Total Harga Emas=======");
+        System.out.printf("Total Bayar Emas : %s %n ", 
+                           mataUangIndonesia.format(
+                           objEmas.perhitungan(objEmas.getHargaGram(), 
+                           objEmas.getBeratEmas())));
+    }
+    
+}
